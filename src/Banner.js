bf8bbeadd64c4cb9bd13a0eaa6e1fdbf7b5d2c23
VACHAN SHETTY <vachan316@gmail.com>
2023-04-17 15:05:07 +0530

    Co-authored-by: Parakh28 <Parakh28@users.noreply.github.com>

--- src/Banner.js
+++ src/Banner.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Banner.css";
 
 function Banner() {
-  return <div>Banner</div>;
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const imageUrls = [
+    "https://firebasestorage.googleapis.com/v0/b/onl-bbms.appspot.com/o/banner%201.jpeg?alt=media&token=891d636c-5cd3-465c-a29a-b6db98289ab2",
+    "https://firebasestorage.googleapis.com/v0/b/onl-bbms.appspot.com/o/banner2.jpg?alt=media&token=ac01e1a2-0008-4642-93c5-4d3d3a9331a9",
+    "https://firebasestorage.googleapis.com/v0/b/onl-bbms.appspot.com/o/Blood-Banks-by-state-March-2018.png?alt=media&token=3eb7eb4b-ed1d-4b79-9e7b-24c681c05557",
+  ];
+
+  function getRandomImageIndex() {
+    return Math.floor(Math.random() * imageUrls.length);
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const randomIndex = getRandomImageIndex();
+      setCurrentImageIndex(randomIndex);
+    }, 10000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="banner">
+      <div className="left-container">
+        {/* Add your features container here */}
+      </div>
+      <div className="right-container">
+        <img src={imageUrls[currentImageIndex]} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
