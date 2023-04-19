f9bd6a771559765f224499ea01ee2c4a2cf28227
Parakh28 <103480164+Parakh28@users.noreply.github.com>
2023-04-19 11:18:47 +0530

    Update HomeScreen.js

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -6,7 +6,8 @@ import Nav from "./Nav";
 function HomeScreen() {
   return (
     <div className="homeScreen">
-      <Nav />
+    <img src="https://firebasestorage.googleapis.com/v0/b/onl-bbms.appspot.com/o/counter-bg.png?alt=media&token=92cdf5be-1064-4736-98fb-ce69ecc18227" />  
+    <Nav />
 
       <Banner />
     </div>
